86112666b8138ce708b15e1b254aa27efcb0219c
Grinko1 <nadenka64013730@gmail.com>
2024-03-15 10:35:09 +0900

    end rest service

--- src/main/java/com/example/catalogService/controller/ProductRestController.java
+++ src/main/java/com/example/catalogService/controller/ProductRestController.java
@@ -1,12 +1,21 @@
 package com.example.catalogService.controller;
 
 import com.example.catalogService.entity.Product;
+import com.example.catalogService.payload.UpdateProductPayload;
 import com.example.catalogService.service.ProductService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.MessageSource;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
+import java.util.NoSuchElementException;
+import java.util.Objects;
 
 @RestController
 @RequiredArgsConstructor
@@ -14,10 +23,46 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProductRestController {
     private final ProductService productService;
     private final MessageSource messageSource;
+
+    @ModelAttribute
+    public Product product(@PathVariable("productId") int productId) {
+        return productService.findProductById(productId).orElseThrow(() -> new NoSuchElementException("catalog.errors.product.not_found"));
+    }
+
     @GetMapping
-    public Product getProductById(){
+    public Product getProductById(@ModelAttribute("product") Product product) {
+        return product;
+    }
+
+    @PatchMapping
+    public ResponseEntity<?> updateProduct(@PathVariable("productId") int productId, @Valid @RequestBody UpdateProductPayload payload, BindingResult bindingResult) throws BindException {
+        if (bindingResult.hasErrors()) {
+            if (bindingResult instanceof BindException exception) {
+                throw exception;
+            } else {
+                throw new BindException(bindingResult);
+            }
+        } else {
+            productService.updateProduct(productId, payload.title(), payload.details());
+            return ResponseEntity.noContent().build();
+        }
 
     }
 
+    @DeleteMapping
+    public ResponseEntity<Void> deleteProduct(@PathVariable("productId") int productId) {
+        productService.deleteProduct(productId);
+        return ResponseEntity.noContent().build();
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<ProblemDetail> handleNoSuchElement(NoSuchElementException exception, Locale locale) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND,
+                        messageSource.getMessage(exception.getMessage(),
+                                new Objects[0], exception.getMessage(),
+                                locale)));
+
+    }
 
 }
--- src/main/java/com/example/catalogService/controller/ProductsRestController.java
+++ src/main/java/com/example/catalogService/controller/ProductsRestController.java
@@ -5,17 +5,13 @@ import com.example.catalogService.payload.NewProductPayload;
 import com.example.catalogService.service.ProductService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.MessageSource;
-import org.springframework.context.MessageSourceResolvable;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
 
 @RestController
@@ -23,7 +19,6 @@ import java.util.Map;
 @RequestMapping("catalog-api/products")
 public class ProductsRestController {
     private final ProductService productService;
-    private final MessageSource messageSource;
 
     @GetMapping
     public List<Product> allProducts() {
@@ -31,12 +26,13 @@ public class ProductsRestController {
     }
 
     @PostMapping
-    public ResponseEntity<?> createProduct(@Valid @RequestBody NewProductPayload payload, BindingResult bindingResult, UriComponentsBuilder uriComponentsBuilder, Locale locale) {
+    public ResponseEntity<?> createProduct(@Valid @RequestBody NewProductPayload payload, BindingResult bindingResult, UriComponentsBuilder uriComponentsBuilder) throws BindException {
         if (bindingResult.hasErrors()) {
-            ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, this.messageSource.getMessage("errors.400.title", new Object[0],"errors.400.title", locale));
-            problemDetail.setProperty("errors",
-                    bindingResult.getAllErrors().stream().map(MessageSourceResolvable::getDefaultMessage).toList());
-            return ResponseEntity.badRequest().body(problemDetail);
+            if (bindingResult instanceof BindException exception) {
+                throw exception;
+            } else {
+                throw new BindException(bindingResult);
+            }
         } else {
             Product product = productService.save(payload.title(), payload.details());
             return ResponseEntity
