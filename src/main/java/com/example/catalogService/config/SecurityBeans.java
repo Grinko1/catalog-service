0376560a4e193c653dbd9e8f5a7c31825d0d30e2
Grinko1 <nadenka64013730@gmail.com>
2024-03-16 12:05:36 +0900

    add security

---
+++ src/main/java/com/example/catalogService/config/SecurityBeans.java
@@ -0,0 +1,23 @@
+package com.example.catalogService.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityBeans {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(request ->
+                request.requestMatchers("/catalog-api/**")
+                        .hasRole("SERVICE"))
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(sessionManagement -> sessionManagement
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return http.build();
+    }
+}
